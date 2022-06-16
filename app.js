eebf17043adcc9cb132224f712c08375667ddfe8
Hoang Nam Trinh <hoangnamtrinh15@gmail.com>
2022-06-16 14:12:59 +0930

    add sql

--- app.js
+++ app.js
@@ -3,10 +3,22 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var mysql = require('mysql');
+// create a 'pool' (group) of connections to be used for connecting with our SQL server
+var dbConnectionPool = mysql.createPool({
+  host: 'localhost',
+  database: 'theatre'
+});
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+app.use(function(req, res, next) {
+  req.pool = dbConnectionPool;
+  next();
+});
 
 app.use(logger('dev'));
 app.use(express.json());
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,42 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/getactors', function(req, res, next) {
+  //Connect to the database
+  req.pool.getConnection( function(err,connection) {
+    if (err) {
+      res.sendStatus(500);
+      return;
+    }
+    var query = "SELECT * FROM actor";
+    connection.query(query, function(err, rows, fields) {
+      connection.release(); // release connection
+      if (err) {
+        res.sendStatus(500);
+        return;
+      }
+      res.json(rows); //send response
+    });
+  });
+});
+
+router.post('/addactor', function(req, res, next) {
+  //Connect to the database
+  req.pool.getConnection( function(err,connection) {
+    if (err) {
+      res.sendStatus(500);
+      return;
+    }
+    var query = "INSERT INTO actor (first_name, last_name) VALUES (?, ?)";
+    connection.query(query, [req.body.first_name, req.body.last_name], function(err, rows, fields) {
+      connection.release(); // release connection
+      if (err) {
+        res.sendStatus(500);
+        return;
+      }
+      res.sendStatus(200); //send response
+    });
+  });
+});
+
 module.exports = router;
