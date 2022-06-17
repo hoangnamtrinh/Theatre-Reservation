bee23d8f05b1735c508511642ff39c8c316ce4d9
hoangnamtrinh <68336209+hoangnamtrinh@users.noreply.github.com>
2022-06-17 00:48:39 +0000

    Fri, Jun 17, 2022, 12:48 AM +00:00

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -128,8 +128,7 @@ var vueinst1 = new Vue({
           for (let i = 0; i < vueinst1.oseats.length; i++) {
             let seat_id = vueinst1.oseats[i].Seat_ID;
             let oseat = document.getElementById(seat_id);
-            console.log(oseat);
-            // oseat.className = "seat occupied";
+            oseat.className += "occupied";
           }
       }
       };
