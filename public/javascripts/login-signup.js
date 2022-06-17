d5c0d8169b1d962e2bd51d5259089faf83f5b2ae
hoangnamtrinh <68336209+hoangnamtrinh@users.noreply.github.com>
2022-06-17 05:09:44 +0000

    Fri, Jun 17, 2022, 5:09 AM +00:00

--- public/javascripts/login-signup.js
+++ public/javascripts/login-signup.js
@@ -6,6 +6,7 @@ function login() {
   xhttp.onreadystatechange = function () {
 
     if (this.readyState == 4 && this.status == 200) {
+      
       window.location = '/home.html';
     } else if (this.readyState == 4 && this.status >= 400) {
       alert("Login Failed! Username or Password incorrect.");
