e837373b3e5eee9d53aa2e46d648b568cfdf2953
hoangnamtrinh <68336209+hoangnamtrinh@users.noreply.github.com>
2022-06-17 01:00:49 +0000

    Fri, Jun 17, 2022, 1:00 AM +00:00

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -136,6 +136,18 @@ var vueinst1 = new Vue({
       xhttp.setRequestHeader("Content-type", "application/json");
       xhttp.send(JSON.stringify({ play_id: vueinst1.play_id, date: vueinst1.date, time: vueinst1.time }));
     },
+    addSeats: function(events) {
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+            // res.send()
+          }
+      }
+      };
+      xhttp.open("POST", "users/getoccupiedseats", true);
+      xhttp.setRequestHeader("Content-type", "application/json");
+      xhttp.send(JSON.stringify({ play_id: vueinst1.play_id, date: vueinst1.date, time: vueinst1.time, seats: bookedSeats }));
+    },
     filter: function(events) {
       var xhttp = new XMLHttpRequest();
       xhttp.onreadystatechange = function() {
