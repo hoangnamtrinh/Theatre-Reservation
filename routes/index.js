dc33e4cb1ff61fb6a0d69a54e451a30f50ac0f41
hoangnamtrinh <68336209+hoangnamtrinh@users.noreply.github.com>
2022-06-17 04:48:54 +0000

    Fri, Jun 17, 2022, 4:48 AM +00:00

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile("login.html");
 });
 
 router.get('/login', function(req, res, next) {
